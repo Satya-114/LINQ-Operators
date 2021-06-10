a190490099e5dba6ec43886883adbca648338082
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-10 09:29:53 +0530

    Add files via upload

---
+++ LINQ Select/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Select1
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            //Using Query Syntax
+            List<Employee> basicQuery = (from emp in Employee.GetEmployees()
+                                         select emp).ToList();
+            foreach (Employee emp in basicQuery)
+            {
+                Console.WriteLine($"ID : {emp.ID} Name : {emp.FirstName} {emp.LastName}");
+            }
+            //Using Method Syntax
+            IEnumerable<Employee> basicMethod = Employee.GetEmployees().ToList();
+            foreach (Employee emp in basicMethod)
+            {
+                Console.WriteLine($"ID : {emp.ID} Name : {emp.FirstName} {emp.LastName}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+        
+
