245243e2c61e9877462d4688a7d657a25c1789dc
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-10 09:43:44 +0530

    Add files via upload

---
+++ SelectMany1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_SelectMany2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Using Method Syntax
+            List<string> MethodSyntax = Student.GetStudents().SelectMany(std => std.Programming).ToList();
+            //Using Query Syntax
+            IEnumerable<string> QuerySyntax = from std in Student.GetStudents()
+                                              from program in std.Programming
+                                              select program;
+            //Printing the values
+            foreach (string program in MethodSyntax)
+            {
+                Console.WriteLine(program);
+            }
+            Console.ReadKey();
+        }
+    }
+}
