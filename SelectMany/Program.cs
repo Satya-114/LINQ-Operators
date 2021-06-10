5a012291ecfb32df16e112ab6122c64d6a8a0bb3
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-10 09:37:45 +0530

    Add files via upload

---
+++ SelectMany/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_SelectMany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nameList = new List<string>() { "Pranaya", "Kumar" };
+            IEnumerable<char> methodSyntax = nameList.SelectMany(x => x);
+            foreach (char c in methodSyntax)
+            {
+                Console.Write(c + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
