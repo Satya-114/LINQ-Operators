b66a244a71a1905263b45a710f2140ef827b46bb
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-10 09:41:49 +0530

    Add files via upload

---
+++ Select 1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Select2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> selectQuery = (from emp in Employee.GetEmployees()
+                                                 select new Employee()
+                                                 {
+                                                     FirstName = emp.FirstName,
+                                                     LastName = emp.LastName,
+                                                     Salary = emp.Salary
+                                                 });
+
+            foreach (var emp in selectQuery)
+            {
+                Console.WriteLine($" Name : {emp.FirstName} {emp.LastName} Salary : {emp.Salary} ");
+            }
+
+            //Method Syntax
+            List<Employee> selectMethod = Employee.GetEmployees().
+                                          Select(emp => new Employee()
+                                          {
+                                              FirstName = emp.FirstName,
+                                              LastName = emp.LastName,
+                                              Salary = emp.Salary
+                                          }).ToList();
+            foreach (var emp in selectMethod)
+            {
+                Console.WriteLine($" Name : {emp.FirstName} {emp.LastName} Salary : {emp.Salary} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
